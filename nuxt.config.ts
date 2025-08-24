eb306232e34e69a5da2f906ced504971b8bf2262
Antoune <yopojo41@gmail.com>
2025-08-24 15:39:22 +0300

    Initial project configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   vite: {
     server: {
-      allowedHosts: ['ff-dev-domain.ru.tuna.am']
+      allowedHosts: ["ff-dev-domain.ru.tuna.am"],
     },
     plugins: [
       tailwindcss(),
@@ -17,17 +17,48 @@ export default defineNuxtConfig({
   },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxtjs/color-mode'
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxtjs/color-mode",
+    "nuxt-svgo",
   ],
 
   eslint: {
     config: {
       standalone: false,
     },
+  },
+
+  icon: {
+    customCollections: [
+      // {
+      //   prefix: "brand-games",
+      //   dir: "./layers/shared-ui/app/assets/brand-games",
+      // },
+    ],
+    clientBundle: {
+      includeCustomCollections: true,
+    },
+    serverBundle: {
+      collections: ["material-symbols", "material-symbols-light", "ic"],
+    },
+  },
+
+  fonts: {
+    defaults: {
+      preload: true,
+    },
+    providers: {
+      google: false,
+      googleicons: false,
+    },
+  },
+
+  svgo: {
+    autoImportPath: "./assets/svg/",
+    defaultImport: "component",
   },
 
 })
