8b4376d854f21535d3b522bbfa2d6d0c753b2ca8
Antoune Overgain <yopojo41@gmail.com>
2025-09-29 17:23:34 +0300

    Fix model a page guitar image height on mobile

--- app/pages/modelA-features.vue
+++ app/pages/modelA-features.vue
@@ -30,7 +30,7 @@ const features = [
 <template>
   <div class="space-y-10">
     <PageTitleSection title="Модель А" />
-    <div class="outline-2 outline-dashed outline-primary/50 p-2 h-[20rem] md:h-[20rem] lg:h-[30rem] mb-10 md:mb-20 lg:mb-30 ">
+    <div class="outline-2 outline-dashed outline-primary/50 p-2 h-[13rem] md:h-[20rem] lg:h-[30rem] mb-10 md:mb-20 lg:mb-30 ">
       <NuxtImg src="model-a/general.webp" alt="title" class="w-full h-full object-contain" />
     </div>
 
